a344930fae180ab4c36b7940f968ebb7f6bf0fdc
akbarasadov <akbarasadov4170@gmail.com>
2025-02-06 20:36:18 +0500

    first commit

---
+++ java.js
@@ -0,0 +1,72 @@
+//1
+let month = +prompt("введите число месяца")
+let week = +prompt("введите число  неделю")
+switch (month) {
+    case 1:
+        console.log("январь");
+        break;
+    case 2:
+        console.log("Февраль");
+        break;
+    case 3:
+        console.log("Март");
+        break;
+    case 4:
+        console.log("Апрель");
+        break;
+    case 5:
+        console.log("Май");
+        break;
+    case 6:
+        console.log("Июнь");
+        break;
+    case 7:
+        console.log("Июль");
+        break;
+    case 8:
+        console.log("Август");
+        break;
+    case 9:
+        console.log("Сентябрь");
+        break;
+    case 10:
+        console.log("Октябрь");
+        break;
+    case 11:
+        console.log("Ноябрь");
+        break;
+    case 12:
+        console.log("Декабрь");
+        break;
+    default:
+        console.log("ошибка");
+        break;
+}
+
+//2
+switch (week) {
+    case 1:
+        console.log("Понедельник");
+        break;
+    case 2:
+        console.log("Вторник");
+        break;
+    case 3:
+        console.log("Среда");
+        break;
+    case 4:
+        console.log("Четверг");
+        break;
+    case 5:
+        console.log("Пятница");
+        break;
+    case 6:
+        console.log("Суббота");
+        break;
+    case 7:
+        console.log("Воскресенье");
+        break;
+    default:
+        console.log("ошибка");
+        break;
+}
